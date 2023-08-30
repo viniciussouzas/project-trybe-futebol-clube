b5f6b9d5f1bf851e7c1868d5256b8a6819f43a86
Vinícius Souza <tradutorvinicius@gmail.com>
2023-08-30 15:42:21 -0300

    Remove testes de dentro da pasta integration, para serem reconhecidos no avaliador

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -3,9 +3,9 @@ import * as chai from 'chai';
 // @ts-ignore
 import chaiHttp = require('chai-http');
 
-import { app } from '../../app';
-import Team from '../../database/models/TeamModel';
-import teamsMocks from '../mocks/teams.mocks';
+import { app } from '../app';
+import Team from '../database/models/TeamModel';
+import teamsMocks from './mocks/teams.mocks';
 
 
 chai.use(chaiHttp);
