94dba7c2bc1fada7c43de6de0ed6989b05921e11
Vinícius Souza <tradutorvinicius@gmail.com>
2023-08-30 14:11:31 -0300

    Adiciona testes (TDD) das funções findAll e findByPk do endpoint /teams

---
+++ app/backend/src/tests/services/teams.test.ts
@@ -0,0 +1,39 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../../app';
+import Example from '../../database/models/ExampleModel';
+
+import { Response } from 'superagent';
+import Team from '../../database/models/team.model';
+import teamsMocks from '../mocks/teams.mocks';
+
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Teams tests', () => {
+  afterEach(function() { sinon.restore() });
+
+  it('Testa se a função findAll retorna o status e data esperados', async function() {
+    sinon.stub(Team, 'findAll').resolves(teamsMocks.teams as Team[]);
+
+    const { status, body } = await chai.request(app).get('/teams');
+
+    expect(status).to.be.equal(200);
+    expect(body).to.be.deep.equal(teamsMocks.teams);
+    expect(body.length).to.be.equal(3);
+  })
+
+  it('Testa se a função findByPk retorna o status e data esperados', async function () {
+    sinon.stub(Team, 'findByPk').resolves(teamsMocks.team as Team);
+
+    const { status, body } = await chai.request(app).get('/teams/1');
+
+    expect(status).to.be.equal(200);
+    expect(body).to.be.deep.equal(teamsMocks.team);
+  });
+});
