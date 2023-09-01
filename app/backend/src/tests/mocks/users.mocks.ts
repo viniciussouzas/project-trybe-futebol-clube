76a1ac633f4cc4eb1205ac8dacb0dfd594a84897
Vinícius Souza <tradutorvinicius@gmail.com>
2023-09-01 15:21:31 -0300

    implementa novos testes de integração de users, e corrige o teste passado

--- app/backend/src/tests/mocks/users.mocks.ts
+++ app/backend/src/tests/mocks/users.mocks.ts
@@ -1,7 +1,20 @@
-const mockToken = {
-  token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjU0NTI3MTg5fQ.XS_9AA82iNoiVaASi0NtJpqOQ_gHSHhxrpIdigiT-fc"
-}
+const mockToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjU0NTI3MTg5fQ.XS_9AA82iNoiVaASi0NtJpqOQ_gHSHhxrpIdigiT-fc";
+
+const adminLoginMock = {
+  email: 'admin@admin.com',
+  password: 'secret_admin',
+};
+
+const adminUserMock = {
+  id: 1,
+  username: 'Admin',
+  role: 'admin',
+  email: 'admin@admin.com',
+  password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
+};
 
 export default {
   mockToken,
-}
+  adminLoginMock,
+  adminUserMock,
+};
--- app/backend/src/tests/users.test.ts
+++ app/backend/src/tests/users.test.ts
@@ -1,5 +1,6 @@
 import * as sinon from 'sinon';
 import * as chai from 'chai';
+import * as jwt from 'jsonwebtoken';
 // @ts-ignore
 import chaiHttp = require('chai-http');
 
@@ -16,11 +17,24 @@ describe('Users tests', () => {
   afterEach(function() { sinon.restore() });
 
   it('Testa se a função findOne retorna o status e data esperados, ao inserir login válido', async function() {
-    sinon.stub(User, 'findOne').resolves(usersMocks.mockToken as any);
+    sinon.stub(User, 'findOne').resolves(usersMocks.adminUserMock as User);
+    sinon.stub(jwt, 'sign').returns(usersMocks.mockToken as any);
 
-    const { status, body } = await chai.request(app).post('/login');
+    const { status, body } = await chai.request(app).post('/login').send(usersMocks.adminLoginMock);
 
     expect(status).to.be.equal(200);
-    expect(body).to.be.deep.equal(usersMocks.mockToken);
-  })
+    expect(body).to.have.property('token');
+    expect(body.token).to.be.deep.equal(usersMocks.mockToken)
+  });
+
+  it('Testa se a função findRole retorna o status e data esperados', async function() {
+    sinon.stub(jwt, 'verify').resolves(usersMocks.adminUserMock.email as any);
+    sinon.stub(User, 'findOne').resolves(usersMocks.adminUserMock as User);
+
+    const { status, body } = await chai.request(app).get('/login/role').set('authorization', `Bearer ${usersMocks.mockToken}` );
+
+    expect(status).to.be.equal(200);
+    expect(body).to.have.property('role');
+    expect(body.role).to.be.deep.equal('admin')
+  });
 });
